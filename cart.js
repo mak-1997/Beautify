6e16225e58af710da62ec282e946841de4d210aa
bhavnesh1811 <erbhavnesharora@gmail.com>
2022-09-30 15:57:55 +0530

    day-3 pushed data

--- cart.js
+++ cart.js
@@ -1,3 +1,15 @@
+import {footer} from "./components/footer.js"
+import {inner,navbar} from "./components/navbar.js"
+document.querySelector("#footer").innerHTML = footer();
+
+
+document.querySelector("#inner").innerHTML = inner();
+document.querySelector("#nav").innerHTML = navbar();
+
+
+
+// document.getElementById("items_count").innerText = totalS.length;
+
 const carts = JSON.parse(localStorage.getItem("cart"))
 
 
@@ -21,23 +33,27 @@ const cartappend = (carts) => {
             let title = document.createElement('p');
             title.innerHTML = el.title;
             let ratings = document.createElement('p');
-            ratings.innerHTML = el.ratings;
+            ratings.innerHTML = `Ratings-${el.ratings}`;
             let price = document.createElement('p');
-            price.innerHTML = ` ₹ ${el.price}`;
+            price.innerHTML = `Price- ₹ ${el.price}`;
             let btn2 = document.createElement("button")
             btn2.innerHTML = 'Remove'
             btn2.addEventListener("click", function(event){
                 event.target.parentNode.remove();
                 carts.splice(i, 1);
                 localStorage.setItem("cart",JSON.stringify(carts))
+                
                 total = carts.reduce(function (acc, el) {
                     return acc + Number(el.price);
                   }, 0);
                 document.getElementById("cost").innerText = `₹ ${total}`;
                 document.getElementById("items").innerText = carts.length;
+                
                 cartappend(carts)
                  
             });
+            let totalS = JSON.parse(localStorage.getItem("cart"))
+            document.getElementById("items_count").innerText = totalS.length;
             div1.append(img)
             div2.append(title,ratings,price,btn2)
             div.append(div1,div2)
